c95bb292c1ccc35123a62b41f52623b6ea819a8b
BuildTools <wootety@gmail.com>
2023-02-05 00:32:07 +0800

    Initialized Leaderboard Screen
    - Leaderboard screen can display with working buttons to go back
    - Will add in functioning tables and cells for the scores soon

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,7 +10,7 @@ public class MyGdxGame extends Game {
 	public void create () {
 		this.setScreen(new MainScreen(this));	
 	}
-
+	
 	@Override
 	public void render () {
 		super.render();
--- core/src/com/mygdx/game/Screen/LeaderboardScreen.java
+++ core/src/com/mygdx/game/Screen/LeaderboardScreen.java
@@ -3,20 +3,26 @@ package com.mygdx.game.Screen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.mygdx.game.Leaderboard.Leaderboard;
+import com.mygdx.game.Leaderboard.LeaderboardEntry;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainScreen implements Screen {
-
+public class LeaderboardScreen implements Screen {
     TextureAtlas atlas;
     Skin skin;
     Stage stage;
@@ -24,8 +30,9 @@ public class MainScreen implements Screen {
     SpriteBatch batch;
     Game game;
 
-    public MainScreen(Game game) {
+    public LeaderboardScreen(Game game){
         this.game = game;
+        this.batch = batch;
         this.atlas = new TextureAtlas("comic/skin/comic-ui.atlas");
         this.skin = new Skin(Gdx.files.internal("comic/skin/comic-ui.json"));
 
@@ -34,6 +41,8 @@ public class MainScreen implements Screen {
         this.camera = new OrthographicCamera(screenWidth, screenHeight);
         this.stage = new Stage(new StretchViewport(screenWidth, screenHeight, this.camera));
         camera.update();
+
+        
     }
 
     @Override
@@ -45,49 +54,59 @@ public class MainScreen implements Screen {
         // set table to fill stage
         mainTable.setFillParent(true); 
         // set alignment of contents in table
-        mainTable.top();
-
-        // button creation
-        TextButton playButton = new TextButton("Start", skin);
-        TextButton scoreButton = new TextButton("Leaderboard", skin);
-        TextButton instructionsButton = new TextButton("Instructions", skin);
-        TextButton exitButton = new TextButton("Exit", skin);
-
-        // add listeners to buttons
-        // create new game screen if clicked
-        playButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent inputEvent, float x, float y){
-                game.setScreen(new GameScreen());
-            }
-        });
-        // enter leaderboard screen if clicked
-        scoreButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent inputEvent, float x, float y){
-                game.setScreen(new LeaderboardScreen(game));
-            }
-        });
+        mainTable.center();
         
-        // exit when exit button is clicked
-        exitButton.addListener(new ClickListener(){
+        // button creation
+        TextButton backButton = new TextButton("Go Back", skin);
+
+        Leaderboard scoreBoard = new Leaderboard();
+
+        // Mock Samples for testing
+        LeaderboardEntry entry1 = new LeaderboardEntry("John", 50);
+        LeaderboardEntry entry2 = new LeaderboardEntry("Bobby", 30);
+        LeaderboardEntry entry3 = new LeaderboardEntry("Gabe", 60);
+        
+        scoreBoard.reviseScoreboard(entry1);
+        scoreBoard.reviseScoreboard(entry2);
+        scoreBoard.reviseScoreboard(entry3);
+        System.out.println(scoreBoard);
+        // End of mock samples and code
+
+        TextField scoreList = new TextField(scoreBoard.toString(), skin);
+        Table scoreTable = new Table(skin);
+        scoreTable.setFillParent(true);
+        scoreTable.top();
+        // add listeners to buttons
+        // go to main screen if clicked
+        backButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent inputEvent, float x, float y){
-                Gdx.app.exit();
+                game.setScreen(new MainScreen(game));
             }
         });
 
         // add buttons to table
-        mainTable.add(playButton);
+        scoreTable.add("Top 10 Scores");
+        scoreTable.row();
+        scoreTable.add(scoreList);
+
         mainTable.row();
-        mainTable.add(scoreButton);
-        mainTable.row();
-        mainTable.add(instructionsButton);
-        mainTable.row();
-        mainTable.add(exitButton);
+        mainTable.add(backButton);
+
+
+        stage.addActor(scoreTable);
         stage.addActor(mainTable);
     }
+    
 
+    // BitmapFont font = new BitmapFont();
+
+    // public void draw(SpriteBatch batch, float parentAlpha){
+    //     font.setColor(238,130,238,1);
+    //     font.draw(batch,"Score", parentAlpha, parentAlpha);
+    //     System.out.println("Draw success");
+    // }
+    
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
