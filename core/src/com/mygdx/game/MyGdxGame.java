bc456708579c209441fb377bdadcc01dc000eeab
alloygoh <alloyg99@gmail.com>
2023-02-06 17:45:30 +0800

    refactor: update screen creation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,9 +2,11 @@ package com.mygdx.game;
 
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.mygdx.game.Manager.SettingsManager;
 import com.mygdx.game.Screen.LoadingScreen;
 import com.mygdx.game.Screen.MainScreen;
 import com.mygdx.game.Utils.Globals;
@@ -31,14 +33,14 @@ public class MyGdxGame extends Game implements LoadingScreen.OnLoadListener {
 		// load other assets
 		assetManager.load("comic/skin/comic-ui.atlas",TextureAtlas.class);
 		assetManager.load("comic/skin/comic-ui.json",Skin.class);
-    this.settingsManager = new SettingsManager(this);
-		settingsManager.readFromConfig();
+		this.settingsManager = new SettingsManager(this);
+		this.settingsManager.readFromConfig();
 	}
 
 	// when assets are done loading
 	@Override
 	public void onLoad() {
-		this.setScreen(new MainScreen(this));
+		this.setScreen(new MainScreen(this, settingsManager));
 	}
 	
 	@Override
