8341b1226b284f5fc3d992f56d17f6e773c35483
alloygoh <alloyg99@gmail.com>
2023-02-07 22:24:01 +0800

    cleanup: remove unused imports

--- core/src/com/mygdx/game/Screen/LoadingScreen.java
+++ core/src/com/mygdx/game/Screen/LoadingScreen.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
