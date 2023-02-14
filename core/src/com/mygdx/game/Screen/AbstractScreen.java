dd26817670bb70d6fb81146f235fcfea8dfd3f24
alloygoh <alloyg99@gmail.com>
2023-02-14 13:09:04 +0800

    refactor: remove unused imports

--- core/src/com/mygdx/game/Screen/AbstractScreen.java
+++ core/src/com/mygdx/game/Screen/AbstractScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public abstract class AbstractScreen implements Screen{
